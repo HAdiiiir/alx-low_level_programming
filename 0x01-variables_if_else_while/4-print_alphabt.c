9355765aca29c3a9933c76319f24b4f9cd95bc82
HAdiiiir <hader.jaamal2010@gmail.com>
2023-10-08 12:56:18 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 97; i <123; i++)
+	for (i = 97; i < 123; i++)
 	{
 		if (i != 101 && i !=113)
 		{
@@ -20,4 +20,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}	
+}
