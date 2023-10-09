c84208ddd92152d4c2cf8d1ae6ad92367f7944c4
HAdiiiir <hader.jaamal2010@gmail.com>
2023-10-09 14:48:31 -0700

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,6 @@
 int main(void)
 {
 	printf("size of a char: %zu byte(s)\n", (unsigned long)sizeof(char));
-
 	printf("size of an int: %zu byte(s)\n", (unsigned long) sizeof(int));
 	printf("size of a long int: %zu bytes(s)\n", (unsigned long)sizeof(long int));
 	printf("size of a long long int: %zu bytes(s)\n", (unsigned long)sizeof(long long int));
