9b8ad92da2028f384aa91c999a4457c96796c7dd
HAdiiiir <hader.jaamal2010@gmail.com>
2023-10-08 22:06:34 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,13 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
- */
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (success)
+*/
+
 int main(void)
 {
-	puts("\ "Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
