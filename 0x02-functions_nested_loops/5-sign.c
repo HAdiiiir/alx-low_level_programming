31f3a26d03d898cf38ed6bd536979257517a29fb
HAdiiiir <hader.jaamal2010@gmail.com>
2023-10-11 11:47:44 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * _islower - checks for lowercase character
  * @c: The character to be checked
- * Return: 1 for lowercase character or 0 for anything else
+ * Return: 1 for positive num, -1 for negative num or zero anything else
  */
 
 int print_sign(int n)
@@ -11,7 +11,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 	_putchar(43);
-return (0);
+return (1);
 	}
 	else if (n < 0)
 	{
@@ -20,7 +20,7 @@ return (0);
 	}
 	else
 	{
-		_putchar(48);
+		_putchar(40);
 		return (0);
 	}
 }
