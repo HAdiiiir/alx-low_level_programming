d12e6ddf7906acc1f4b1ac755ce167e1492023ef
HAdiiiir <hader.jaamal2010@gmail.com>
2023-10-11 17:01:29 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,19 @@
-#include "main.h"
-
-/**
- * main - print_alphabet - print all alphabet in lowercase
+/*
+ * File: 1-alphabet.c
+ * Auth: Brennan D Baraban
  */
 
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
 void print_alphabet(void)
 {
-char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-	_putchar(letter);
+	char letter;
 
-_putchar('\n');
-}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}}
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _islower - checks for lowercase character
  * @c: The character to be checked
  * Return: 1 for positive num, -1 for negative num or zero anything else
+ * print_sign - entry point
  */
 
 int print_sign(int c)
