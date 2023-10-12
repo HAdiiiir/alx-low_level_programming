54ed380b568d1153883e19e3493721d96b37bd33
HAdiiiir <hader.jaamal2010@gmail.com>
2023-10-11 17:26:59 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,33 +1,43 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (success)
-*/
-
+ * main - a simple program that outputs unordered
+ * combinations of two digit integers without duplicates
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-int n, m;
+	int ifirst;
+	int i;
+	int jfirst;
+	int j;
 
-for (n = 0; n <= 98; n++)
-{
-for (m = 0; m <= 99; m++)
-{
-if (m > n)
-{
-putchar((n / 10) + 0);
-putchar((n % 10) + 0);
-putchar(' ');
-putchar((n / 10) + 0);
-putchar((n % 10) + 0);
-if (n != 98 || m != 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	for (ifirst = 48; ifirst < 58; ifirst++)
+	{
+		for (i = 48; i < 58; i++)
+		{
+			j = i + 1;
+			jfirst = ifirst;
+			for (; jfirst < 58; jfirst++)
+			{
+				for (; j < 58; j++)
+				{
+					putchar(ifirst);
+					putchar(i);
+					putchar(' ');
+					putchar(jfirst);
+					putchar(j);
+					if (ifirst != 57 || jfirst != 57 || i != 56 || j != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				j = 48;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
