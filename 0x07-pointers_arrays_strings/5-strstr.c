cdff18f31149d6ca1d7fd0828836cb58ec4052d4
HAdiiiir <hader.jaamal2010@gmail.com>
2023-10-23 13:45:01 -0700

    a

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,21 +1,43 @@
+#include <stdlib.h>
 /**
- * _strcat - concatenates two strings
- * @dest: pointer to the destination string
- * @src: pointer to the source string
- * Return: pointer to the resulting string dest
+ * _strncmp - compares two strings within b bytes.
+ * @s1: string to check
+ * @s2: string to check from
+ * @b: number of bytes to check
+ * Return: 0 if strings are the same
+ * any other number if they are different.
  */
-
-char *_strcat(char *dest, char *src)
+int _strncmp(char *s1, char *s2, unsigned int b)
 {
-	int i, j;
+	unsigned int i;
 
-	i = 0;
-	for (j = 0; dest[j] != '\0'; j++)
-		;
-	while (src[i] != '\0')
+	for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
+
 	{
-		dest[j + i] = src[i];
-		i++;
+		s1++;
+		s2++;
 	}
-	return (dest);
+	return (i - b);
+}
+/**
+ * _strstr - locates a substring
+ * @haystack: string to look though
+ * @needle: substring to find
+ * Return: pointer to string with result of search
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int len;
+	/*Get length of needle for strncmp*/
+	len = 0;
+	while (needle[len] != '\0')
+		len++;
+	/*compare substring*/
+	while (*haystack != '\0')
+	{
+		if (_strncmp(haystack, needle, len) == 0)
+			return (haystack);
+		haystack++;
+	}
+	return (NULL);
 }
