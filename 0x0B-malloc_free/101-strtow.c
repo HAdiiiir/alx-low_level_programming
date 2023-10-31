4b7dc447fe3b816c5684c76aff57b385fe4d5527
HAdiiiir <hader.jaamal2010@gmail.com>
2023-10-31 13:16:51 -0700

    a

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,20 +10,20 @@
  */
 int number(char *str)
 {
-int a, num = 0;
+	int a, num = 0;
 
-for (a = 0; str[a] != '\0'; a++)
+	for (a = 0; str[a] != '\0'; a++)
 	{
-if (*str == ' ')
-str++;
-else
+		if (*str == ' ')
+			str++;
+		else
 		{
-for (; str[a] != ' ' && str[a] != '\0'; a++)
-str++;
-num++;
+			for (; str[a] != ' ' && str[a] != '\0'; a++)
+				str++;
+			num++;
 		}
 	}
-return (num);
+	return (num);
 }
 /**
  * free_everything - frees the memory
@@ -32,10 +32,11 @@ return (num);
  */
 void free_everything(char **string, int i)
 {
-for (; i > 0;)
-free(string[--i]);
-free(string);
+	for (; i > 0;)
+		free(string[--i]);
+	free(string);
 }
+
 /**
  * strtow - function that splits string into words
  * @str: string being passed
@@ -43,43 +44,44 @@ free(string);
  */
 char **strtow(char *str)
 {
-int total_words = 0, b = 0, c = 0, length = 0;
-char **words, *found_word;
+	int total_words = 0, b = 0, c = 0, length = 0;
+	char **words, *found_word;
 
-if (str == 0 || *str == 0)
-return (NULL);
-total_words = number(str);
-if (total_words == 0)
-return (NULL);
-words = malloc((total_words + 1) * sizeof(char *));
-if (words == 0)
-return (NULL);
-for (; *str != '\0' &&  b < total_words;)
+	if (str == 0 || *str == 0)
+		return (NULL);
+	total_words = number(str);
+	if (total_words == 0)
+		return (NULL);
+	words = malloc((total_words + 1) * sizeof(char *));
+	if (words == 0)
+		return (NULL);
+	for (; *str != '\0' &&  b < total_words;)
 	{
-if (*str == ' ')
-str++;
-else
+		if (*str == ' ')
+			str++;
+		else
 		{
-found_word = str;
-for (; *str != ' ' && *str != '\0';)
+			found_word = str;
+			for (; *str != ' ' && *str != '\0';)
 			{
-length++;
-str++;
+				length++;
+				str++;
 			}
-words[b] = malloc((length + 1) * sizeof(char));
-if (words[b] == 0)
+			words[b] = malloc((length + 1) * sizeof(char));
+			if (words[b] == 0)
 			{
-free_everything(words, b);
-return (NULL);
+				free_everything(words, b);
+				return (NULL);
 			}
-while (*found_word != ' ' && *found_word != '\0')
+			while (*found_word != ' ' && *found_word != '\0')
 			{
-words[b][c] = *found_word;
-found_word++;
-c++;
+				words[b][c] = *found_word;
+				found_word++;
+				c++;
 			}
-words[b][c] = '\0';
+			words[b][c] = '\0';
+			b++; c = 0; length = 0; str++;
 		}
 	}
-return (words);
+	return (words);
 }
