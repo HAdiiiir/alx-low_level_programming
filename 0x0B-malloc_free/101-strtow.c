273918681fc00d48cc4ad8a6fc4597499facd849
HAdiiiir <hader.jaamal2010@gmail.com>
2023-10-31 20:12:37 -0700

    a

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,86 +1,36 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
-/**
- * number - function to calculate number of words
- * @str: string being passed to check for words
- *
- * Return: number of words
- */
-int number(char *str)
+#include <stdlib.h>
+#include <string.h>
+
+char *str_concat(char *s1, char *s2)
 {
-	int a, num = 0;
+int i, j, len1 = 0, len2 = 0;
+char *ptr;
 
-	for (a = 0; str[a] != '\0'; a++)
-	{
-		if (*str == ' ')
-			str++;
-		else
-		{
-			for (; str[a] != ' ' && str[a] != '\0'; a++)
-				str++;
-			num++;
-		}
-	}
-	return (num);
-}
-/**
- * free_everything - frees the memory
- * @string: pointer values being passed for freeing
- * @i: counter
- */
-void free_everything(char **string, int i)
-{
-	for (; i > 0;)
-		free(string[--i]);
-	free(string);
-}
-/**
-* **strtow - function that splits a string into words.
-* @str : pointer
-* Return: Char
-*/
+if (s1 == NULL && s2 == NULL)
+return (NULL);
 
-char **strtow(char *str)
-{
-	int total_words = 0, b = 0, c = 0, length = 0;
-	char **words, *found_word;
+if (s1 == NULL)
+s1 = "";
 
-	if (str == 0 || *str == 0)
-		return (NULL);
-	total_words = number(str);
-	if (total_words == 0)
-		return (NULL);
-	words = malloc((total_words + 1) * sizeof(char *));
-	if (words == 0)
-		return (NULL);
-	for (; *str != '\0' &&  b < total_words;)
+if (s2 == NULL)
+s2 = "";
 
-		if (*str == ' ')
-			str++;
-		else
-		{
-			found_word = str;
-			for (; *str != ' ' && *str != '\0';)
-			{
-				b++;
-				length = 0;
-				str++;
-				c = 0;
-			}
-			words[b] = malloc((length + 1) * sizeof(char));
-			if (words[b] == 0)
-			{
-				free_everything(words, b);
-				return (NULL);
-			}
-			while (*found_word != ' ' && *found_word != '\0')
-			{
-				words[b][c] = *found_word;
-				found_word++;
-				c++;
-			}
-			words[b][c] = '\0';
-		}
-	return (words);
+while (s1[len1] != '\0')
+len1++;
+while (s2[len2] != '\0')
+len2++;
+
+ptr = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+
+if (ptr == NULL)
+return (NULL);
+for (i = 0; i < len1; i++)
+ptr[i] = s1[i];
+for (j = 0; j < len2; j++)
+ptr[i + j] = s2[j];
+
+ptr[i + j] = '\0';
+
+return (ptr);
 }
