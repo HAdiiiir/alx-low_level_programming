9bd4790d82089c8f3cffe4a813b1170a2a5d99bc
Hadeer Jaamal <133434041+HAdiiiir@users.noreply.github.com>
2023-11-16 18:36:10 -0800

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,26 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_list - Free all malloced spaced of a list
- * @head: Pointer to the start of the list
- */
+ * free_list - frees a list_t list.
+ * @head: pointer to the list.
+ **/
 void free_list(list_t *head)
 {
-	if (head != NULL)
-	{
-		if (head->next != NULL)
-			free_list(head->next);
-		free(head->str);
-		free(head);
-	}
+    list_t *actual_node;
+    list_t *next_node;
+
+    if (head)
+    {
+        actual_node = head;
+        next_node = head->next;
+        while (next_node)
+        {
+            free(actual_node->str);
+            free(actual_node);
+            actual_node = next_node;
+            next_node = next_node->next;
+        }
+        free(actual_node->str);
+        free(actual_node);
+    }
 }
