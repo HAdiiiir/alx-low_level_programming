ad6201dbe54419f3d47c7daa769cdd82da596c45
HAdiiiir <hader.jaamal2010@gmail.com>
2023-11-17 01:50:51 -0800

    a

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,19 @@
-section .data
-    hello db 'Hello, Holberton', 0
+    section .data
+hello:	db "Hello, Holberton",10,0
+format:	db "%s",0
 
-section .text
+    section .text
     global main
     extern printf
 
 main:
-    ; Prepare the arguments for printf
-    mov rdi, hello     ; The format string
-    xor rax, rax       ; 0 floating point parameters
-    ; Call printf
-    call printf
-    ; Return 0
-    mov eax, 0x60      ; The syscall number for sys_exit
-    xor edi, edi       ; Exit code 0
-    syscall            ; Call the kernel
+    push rbp
+    mov rbp, rsp
 
+    mov rsi, hello
+    mov rdi, format
+    call printf
+
+    pop rbp
+    mov rax, 0
+    ret
